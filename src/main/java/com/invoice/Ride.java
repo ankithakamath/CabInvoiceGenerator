68351b1841f59c5e1e02d5fba7f69d05b4351069
Ankitha Kamath <ankithakamath@196AMBBLR-Ankitha-Kamath.local>
2021-09-29 00:47:28 +0530

    The method to Generate fare of Multiple rides is added

---
+++ src/main/java/com/invoice/Ride.java
@@ -0,0 +1,11 @@
+package com.invoice;
+
+public class Ride {
+	public double distance;
+	public int time;
+
+	public Ride(double distance, int time) {
+		this.time = time;
+		this.distance = distance;
+	}
+}
